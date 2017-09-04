8a1dd59a343ec83dc62ab8ab6d1cb5bfadf5f087
adamtre9115 <treadam9115@gmail.com>
2017-09-04 14:50:31 -0400

    update to game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,62 @@
 // array of artists from 2000s
 var artists = ["Aaliyah", "Alicia Keys", "Jagged Edge", "Pharrell", "Beyonce", "Usher", "Terror Squad", "Yung Joc", "R Kelly", "50 Cent", "Maxwell"]
+var word ;
 // Guesses in game
 var guessRemain = 15;
+var guessed = [];
 var wins = 0;
 var losses = 0;
 
+// run game when any key is pressed
 
-// randomly choose an artist
-var artistChoice = artists[Math.floor(Math.random() * artists.length)];
+document.onkeyup = function(event) {
+    // randomly choose an artist
+        newArtist();
+    // number of guesses start at 15 and decrease
+        guessCount();
+        
+    // display underscores in amount of random artist name
+        underscore();
+    // as guess is correct display letter 
 
+    // guess count decrease as guess continues
+        
+    // display guessed letters
 
-// display _ _ _ in amount of artist name
+   
+} 
 
-// as guess is correct display letter 
+// randomly select new artist
+function newArtist(){
+    var artistChoice = artists[Math.floor(Math.random() * artists.length)];
+    word = artistChoice;
+    console.log(word);
+}
 
-// guess count decrease as guess continues
+// guess count starts at 15 and decreases on each guess
+function guessCount(){
+    var count = document.querySelector("#guessLeft");
+    count.innerHTML = guessRemain;
+    count.onkeyup = guessRemain -= 1;
+}
 
-// display guessed letters
+// display underscores in amount of random artist name
+function underscore() {
+   var wordSpace = document.querySelector("#hold");
+   var newUL = document.createElement("ul");
 
-// number of guesses start at 15
+   for (var i = 0; i < word.length; i++){
+       newUL.setAttribute("id", "wordHold");
+       var space = document.createElement('li');
+       space.setAttribute('class', 'guess');
+       if (word[i] === "-") {
+         space.innerHTML = "-";
+       }else {
+           space.innerHTML = "_";
+       }
+
+       guessed.push(space);
+       wordSpace.appendChild(newUL);
+       newUL.appendChild(space);
+   }
+}
